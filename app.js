f5c7b33ba560540bf8e5b92ac62eeb2da3ecd001
Eliyas Wubie <eliyas.wubie1@gmail.com>
2024-07-08 00:21:46 -0700

    sec

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('message', (message) => {
+        console.log('Received message:', message);
+        // Echo the message back to the client
+        socket.emit('message', `Echo: ${message}`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
